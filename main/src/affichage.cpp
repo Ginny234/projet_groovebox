3ed9106c638081b2889ea82c9992595119551d05
Ginny234 <reypersonne@gmail.com>
2026-06-25 15:53:42 +0200

    plus d'affichage pitch

--- main/src/affichage.cpp
+++ main/src/affichage.cpp
@@ -47,19 +47,6 @@ void affichage_base(){//tous menus compris, afficher absolument TOUT le temps
   
   display.setCursor(SCREEN_WIDTH-strlen(vol)*6, 0);
   display.print(vol);
-  
-  // Affichage du Pitch
-  char pitch_str[20];
-  sprintf(pitch_str, "Pitch: %.2f", pitch_courant);
-  display.setTextSize(2);
-  display.setCursor(0, 12);
-  display.print(pitch_str);
-  display.setTextSize(1);
-
-  char adc_str[16];
-  sprintf(adc_str, "A7:%d", pitch_brut_courant);
-  display.setCursor(0, 40);
-  display.print(adc_str);
 
   display.display();
 }
